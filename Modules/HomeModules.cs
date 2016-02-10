59af591f4fec9344d5079756794771019a4d388c
michael smith <michaelanthonysmith00@gmail.com>
2016-02-10 14:49:05 -0800

    initial commit, set up sit where user can make a pet and kill it!

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,38 @@
+using Nancy;
+using Tamagotchi.Objects;
+using System.Collections.Generic;
+
+namespace Tamagotchi
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        List<Pet> allPets = Pet.GetAll();
+        if (allPets.Count >=0)
+        {
+          return View["index.cshtml",allPets];
+        }
+        else
+        {
+          return View["index.cshtml"];
+        }
+      };
+      Post["/"] = _ => {
+        Pet newPet = new Pet(Request.Form["new-pet"]);
+        List<Pet> allPets = Pet.GetAll();
+        return View["index.cshtml", allPets];
+      };
+      Post["/time"]= _ =>{
+        Pet.TimePass();
+        List<Pet> allPets = Pet.GetAll();
+        return View["index.cshtml", allPets];
+      };
+      Get["/pets/{id}"] = parameters => {
+        Pet pet = Pet.Find(parameters.id);
+        return View["/pet.cshtml", pet];
+      };
+    }
+  }
+}
---
+++ Objects/Pet.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+
+namespace Tamagotchi.Objects
+{
+  public class Pet
+  {
+    private string _name;
+    private int _food;
+    private int _attention;
+    private int _rest;
+    private int _id;
+    private static List<Pet> _pets = new List<Pet> {};
+    private bool _status;
+
+    public Pet(string name, int food = 100, int attention = 100, int rest = 100, bool status = true)
+    {
+      _name = name;
+      _status = status;
+      _food = food;
+      _attention = attention;
+      _rest = rest;
+      _pets.Add(this);
+      _id = _pets.Count;
+    }
+    public bool GetStatus()
+    {
+      int food = GetFood();
+      int attention = GetAttention();
+      int rest = GetRest();
+      if (food <= 0 || attention <=0 || rest <=0){
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
+    public void SetStatus(bool status)
+    {
+      _status = status;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string name)
+    {
+      _name = name;
+    }
+    public int GetFood()
+    {
+      return _food;
+    }
+    public void SetFood(int food)
+    {
+      _food = food;
+    }
+    public int GetAttention()
+    {
+      return _attention;
+    }
+    public void SetAttention(int attention)
+    {
+      _attention = attention;
+    }
+    public int GetRest()
+    {
+      return _rest;
+    }
+    public void SetRest(int rest)
+    {
+      _rest = rest;
+    }
+    public static List<Pet> GetAll()
+    {
+      return _pets;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static Pet Find(int id)
+    {
+      return _pets[id-1];
+    }
+
+    public static void TimePass()
+    {
+      List<Pet> pets = Pet.GetAll();
+      foreach(Pet pet in pets)
+      {
+        int food = pet.GetFood();
+        pet.SetFood(food - 10);
+      }
+    }
+  }
+}
